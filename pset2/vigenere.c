20847dfc3d52da42dc4402c2da6922e7009a98cf
mayank6 <chauhanmayank.621@gmail.com>
2018-07-03 16:17:36 +0530

    Add files via upload

---
+++ pset2/vigenere.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(int argc,char *argv[]){
+    if(argc==2){
+    int k=0,y=0,i;
+    string x=argv[1];
+    string key=x;
+     while(y<strlen(x)) {
+      key[y]=tolower(x[y]);
+      y++;
+   }
+    string text=get_string();
+    for(i=0,n=strlen(text);i<n;i++){
+            if (isalpha(text[i])){
+                if (isupper(text[i])){
+                    printf("%c", (((text[i] + ((key[k]-97)%26)) - 65) % 26) + 65);
+                }
+                else if(islower(text[i])){
+                    printf("%c", (((text[i] + ((key[k]-97)%26)) - 97) % 26) + 97);
+                }
+                k=(k+1)%strlen(key);
+            }
+            else{
+                printf("%c",text[i]);
+            }
+    }
+    }
+    else{
+        return 1;
+    }
+}
